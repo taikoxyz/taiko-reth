3a67414f5c866ed76eade16ff9b416a58949757a
Petar Vujovic <petarvujovic98@gmail.com>
2024-03-20 10:11:25 +0100

    fix: Enhance the EthApi trait with Taiko APIs

--- crates/rpc/rpc-api/src/eth.rs
+++ crates/rpc/rpc-api/src/eth.rs
@@ -307,4 +307,20 @@ pub trait EthApi {
         keys: Vec<JsonStorageKey>,
         block_number: Option<BlockId>,
     ) -> RpcResult<EIP1186AccountProofResponse>;
+
+    /// HeadL1Origin returns the latest L2 block's corresponding L1 origin.
+    #[cfg(feature = "taiko")]
+    #[method(name = "headL1Origin")]
+    async fn head_l1_origin(&self) -> RpcResult<reth_primitives::L1Origin>;
+
+    /// L1OriginByID returns the L2 block's corresponding L1 origin.
+    #[cfg(feature = "taiko")]
+    #[method(name = "l1OriginByID")]
+    async fn l1_origin_by_id(&self, block_id: u64) -> RpcResult<reth_primitives::L1Origin>;
+
+    /// GetL2ParentHeaders
+    #[cfg(feature = "taiko")]
+    #[method(name = "getL2ParentHeaders")]
+    async fn get_l2_parent_headers(&self, block_id: u64)
+        -> RpcResult<Vec<reth_primitives::Header>>;
 }
