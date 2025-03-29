54395eda9796a250f10b5c3818e9951278934603
john xu <john@taiko.xyz>
2025-03-29 13:53:25 +0800

    fix(evm): enforce success for anchor transactions in taiko execution

--- crates/ethereum/evm/src/execute.rs
+++ crates/ethereum/evm/src/execute.rs
@@ -300,6 +300,9 @@ where
             // append gas used
             cumulative_gas_used += result.gas_used();
             if is_taiko {
+                if is_anchor && !result.is_success() {
+                    return Err(BlockExecutionError::Other("anchor transaction must be success"));
+                }
                 let mining_gas_limit = taiko_data.clone().unwrap().gas_limit;
                 if cumulative_gas_used > mining_gas_limit {
                     warn!(
